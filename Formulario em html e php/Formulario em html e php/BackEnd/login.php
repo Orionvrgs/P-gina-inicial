b4c9170975ba919aae813d684cdd36056eaa03aa
Orionvrgs <144965082+Orionvrgs@users.noreply.github.com>
2024-08-29 15:16:58 -0300

    Add files via upload

---
+++ Formulario em html e php/Formulario em html e php/BackEnd/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <label for="nome">nome</label>
+
+
+    
+</body>
+</html>
+<?php
+
+    if $_SERVER ()
+?>
